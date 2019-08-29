09cb2227d21a8aa1bd631793abb0528b3e3b9411
Rich Keyzor <web_dev_rich@keyzor.net>
2019-08-29 15:53:48 +0100

    Change output folder to docs

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,11 @@ module.exports = function(config) {
 
   config.addPassthroughCopy('_input/css');
   config.addPassthroughCopy('_input/img');
-  config.addPassthroughCopy('_input/README.md');
-
 
   return {
     dir: {
       input: "_input",
-      output: "_output"
+      output: "docs"
     },
     templateFormats: [
       "md",
--- _output/README.md
+++
@@ -1,4 +0,0 @@
-# webdevrich-site
-Skillcrush Phase 1 Milestone Project - Build &amp; Launch Your Website
-
-Currently work in progress. Will replace the site currently at https://webdevri.ch/.
